7803a02feeb48f788851db5d5cf20ba7518da58b
Kiloa7 <131774447+Kiloa7@users.noreply.github.com>
2023-10-06 18:40:13 +0300

    практика 06.10

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 // echo "changes made from home";
 echo "<img src='http://localhost/леди.png'>";
-//echo php_uname();
-
+echo php_uname();
+/*$a = 'https://catfact.ninja/fact';
+$data = $a->fact;
+echo $data;*/
 ?>
