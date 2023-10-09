692fdba0118cefc91094efb55758e6b2e739c644
Вета <vad.vargas@yandex.ru>
2023-10-09 22:04:10 +0300

    баннер

--- index.php
+++ index.php
@@ -1,8 +1,64 @@
 <?php
 
+$content = "<img src='http://localhost/1.png'>";
+
+	echo <<<HTML
+		<img src='http://localhost/2.png' style = "position:fixed;height:253px"/>
+		<div style = "position:fixed; left:197px; top:185px; font-size: 38px; font">10</div>
+		<div style = "position:absolute; top: 263px">
+		<p>text1</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+		<p>text</p>
+
+		</div>
+		<img src='http://localhost/2.png' style = "position:fixed;height:253px"/>
+		<div style = "position:fixed; left:197px; top:185px; font-size: 38px; font">10</div>
+HTML;
 // echo "changes made from home";
-echo "<img src='http://localhost/леди.png'>";
-echo php_uname();
+//echo "<img src='http://localhost/леди.png'>";
+//echo php_uname();; background-image:'http://localhost/2.png'
 /*$a = 'https://catfact.ninja/fact';
 $data = $a->fact;
 echo $data;*/
