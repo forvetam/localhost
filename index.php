252dea6a80ba36ddba9388a82efd1dab84b65507
Вета <vad.vargas@yandex.ru>
2023-10-12 19:11:55 +0300

    Подключение API

--- index.php
+++ index.php
@@ -1,65 +1,32 @@
 <?php
 
-$content = "<img src='http://localhost/1.png'>";
+$todayte = date('d-m-Y');
+$url = "https://official-joke-api.appspot.com/jokes/programming/random"; 
+$json = json_decode(substr(substr(file_get_contents($url), 1), 0, strlen ($str)-1));
+$setup_joke = $json->setup;
+$punchline_joke = $json->punchline;
 
 	echo <<<HTML
-		<img src='http://localhost/2.png' style = "position:fixed;height:253px"/>
-		<div style = "position:fixed; left:197px; top:185px; font-size: 38px; font">10</div>
-		<div style = "position:absolute; top: 263px">
-		<p>text1</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
-		<p>text</p>
+		<body style="background-color: #062731">
 
-		</div>
-		<img src='http://localhost/2.png' style = "position:fixed;height:253px"/>
-		<div style = "position:fixed; left:197px; top:185px; font-size: 38px; font">10</div>
+			<div  align="center", style = "background-color: #FFFFFF; word-wrap:break-word; width:632px; position:absolute; top: 270px">
+				<p><b>Лог изменений:</b></p>
+				<p>_____________________</p>
+				<p>Использован API c случайными шутками</p>
+				<p>$url</p>				
+				<p>$setup_joke</p>
+				<p>$punchline_joke</p>
+				<p>_____________________</p>
+			</div>
+			<img src='http://localhost/октябрь.png' style = "position:fixed;height:253px"/>
+			<div style = "position:fixed; left:108px; top:131px; font-size: 30px; color: #062731">$todayte</div>
+
+		</body>
 HTML;
 // echo "changes made from home";
 //echo "<img src='http://localhost/леди.png'>";
-//echo php_uname();; background-image:'http://localhost/2.png'
+//echo php_uname();; background-image:'http://localhost/2.png' <p>$json</p>
 /*$a = 'https://catfact.ninja/fact';
 $data = $a->fact;
-echo $data;*/
+echo $data;*/ 
 ?>
--- new.php
+++ new.php
@@ -1,5 +1,10 @@
 <?php
 
-echo "an entirely new file";
+$url = "https://official-joke-api.appspot.com/jokes/programming/random";
+//$json = json_decode(file_get_contents($url));
+//$setup = $json->setup;
+//$punchline = $json->punchline;
+$data = 0;
 
+echo substr(substr(file_get_contents($url), 1), 0, strlen ($str)-1);
 ?>
