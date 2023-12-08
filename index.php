0e4e2a1d78b8806eef3e1401952d12c6ec5b4a15
Kosyga <vad.vargas@yandex.ru>
2023-12-08 18:22:38 +0300

    start_of_something_new

--- index.php
+++ index.php
@@ -1,20 +1,8 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+<body style="background-color: #062731">
 
-$todayte = date('d-m-Y');
-$url = "https://official-joke-api.appspot.com/jokes/programming/random"; 
-$json = json_decode(substr(substr(file_get_contents($url), 1), 0, strlen ($str)-1));
-$setup_joke = $json->setup;
-$punchline_joke = $json->punchline;
-if (PHP_OS === "Linux") { 
-	$pic = "http://localhost/localhost/октябрь.png";
-	}
-else {
-	$pic = "http://localhost/октябрь.png";
-}
-	echo <<<HTML
-		<body style="background-color: #062731">
-
-			<div  align="center", style = "background-color: #FFFFFF; word-wrap:break-word; width:632px; position:absolute; top: 270px">
+			<div  align="center", style = "background-color: #FFFFFF; word-wrap:break-word; width:632px; position:absolute">
 				<p><b>Лог изменений:</b></p>
 				<p>_____________________</p>
 				<p>Использован API c случайными шутками</p>
@@ -23,16 +11,6 @@ else {
 				<p>$punchline_joke</p>
 				<p>_____________________</p>
 			</div>
-			<img src=$pic style = "position:fixed;height:253px"/>
-			<div style = "position:fixed; left:108px; top:131px; font-size: 30px; color: #062731">$todayte</div>
 
 		</body>
-
-HTML;
-// echo "changes made from home";
-//echo "<img src='http://localhost/леди.png'>";
-//echo php_uname();; background-image:'http://localhost/2.png' <p>$json</p>
-/*$a = 'https://catfact.ninja/fact';
-$data = $a->fact;
-echo $data;*/ 
-?>
+</html>
